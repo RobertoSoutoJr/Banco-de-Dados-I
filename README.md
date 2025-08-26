155743237a4c1306683a768b81dd94bb012ad4bd
RobertoSoutoJr <robertosj@unipam.edu.br>
2025-08-26 23:25:53 +0000

    Código da aula do dia 26/08/2025 - Fim da Aula

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Banco-de-Dados-I
 Banco de Dados I - Unipam 4º Período 2025
 git add .
-git commit -m "Código da aula do dia 01/01/2001 - Tema ABCDE"
+git commit -m "Código da aula do dia 26/08/2025 - Fim da Aula"
 git push origin main
--- aula01/schema.sql
+++ aula01/schema.sql
@@ -35,5 +35,18 @@ CREATE TABLE Venda(
     usuario_id INTEGER NOT NULL,
     cliente_id INTEGER NOT NULL,
     FOREIGN KEY (usuario_id) REFERENCES usuario(id) ON DELETE SET NULL,
-    FOREIGN KEY (cliente_id) REFERENCES cliente(id) ON DELETE SET NULL,
+    FOREIGN KEY (cliente_id) REFERENCES cliente(id) ON DELETE SET NULL
+);
+
+-- Criando TABELA venda_Produto / tabela associatia para N:M [produto <-> venda]
+
+CREATE TABLE venda_Produto (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    venda_id INTEGER NOT NULL,
+    produto_id INTEGER NOT NULL,
+    quantidade INTEGER NOT NULL CHECK (quantidade > 0),
+    preco_unitario REAL NOT NULL CHECK (preco_unitario > 0),
+    --PRIMARY KEY (venda_id, produto_id), -- CHAVE COMPOSTA
+    FOREIGN KEY (venda_id) REFERENCES venda(id) ON DELETE CASCADE,
+    FOREIGN KEY (produto_id) REFERENCES produto(id) ON DELETE CASCADE
 );
