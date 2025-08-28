84e195db4804fb4bc461c464e70340138a7c0563
RobertoSoutoJr <robertosj@unipam.edu.br>
2025-08-28 01:01:11 +0000

    Código da aula do dia 27/08/2025 - Fim da Aula

--- aula01/schema.sql
+++ aula01/schema.sql
@@ -50,3 +50,55 @@ CREATE TABLE venda_Produto (
     FOREIGN KEY (venda_id) REFERENCES venda(id) ON DELETE CASCADE,
     FOREIGN KEY (produto_id) REFERENCES produto(id) ON DELETE CASCADE
 );
+
+
+
+-- Inserindo Dados na TABELA Usuario
+-- Venda 5: Paulo comprou
+INSERT INTO venda_Produto (venda_id, produto_id, quantidade, preco_unitario) VALUES
+(5, 5, 1, 149.90),    -- 1 Relógio Digital
+(5, 7, 1, 349.90);    -- 1 Jaqueta de Couro
+
+-- Venda 6: Juliana comprou
+INSERT INTO venda_Produto (venda_id, produto_id, quantidade, preco_unitario) VALUES
+(6, 6, 1, 249.90),    -- 1 Fone Bluetooth
+(6, 8, 2, 99.90);     -- 2 Óculos de Sol
+
+-- Venda 7: João comprou novamente
+INSERT INTO venda_Produto (venda_id, produto_id, quantidade, preco_unitario) VALUES
+(7, 2, 1, 129.90),    -- 1 Calça Jeans
+(7, 5, 1, 149.90);    -- 1 Relógio Digital
+
+
+-- Inserindo Dados na TABELA Cliente
+INSERT INTO Cliente (nome, telefone, usuario_id) VALUES
+('Paulo Mendes', '11911112222', 4),
+('Juliana Costa', '11933334444', 5);
+
+-- Inserindo Dados na TABELA Produto
+INSERT INTO Produto (nome, descricao, preco) VALUES
+('Relógio Digital', 'Relógio resistente à água', 149.90),
+('Fone Bluetooth', 'Fone de ouvido sem fio', 249.90),
+('Jaqueta de Couro', 'Jaqueta preta tamanho G', 349.90),
+('Óculos de Sol', 'Óculos com proteção UV', 99.90);
+
+-- Inserindo Dados na TABELA Venda
+INSERT INTO Venda (data, usuario_id, cliente_id) VALUES
+('2025-08-26', 4, 4), -- Paulo com Fernanda
+('2025-08-27', 5, 5), -- Juliana com Ricardo
+('2025-08-27', 2, 1); -- João novamente com Ana
+
+-- Inserindo Dados na TABELA venda_Produto
+INSERT INTO venda_Produto (venda_id, produto_id, quantidade, preco_unitario) VALUES
+(5, 5, 1, 149.90),    -- 1 Relógio Digital
+(5, 7, 1, 349.90);    -- 1 Jaqueta de Couro
+
+INSERT INTO venda_Produto (venda_id, produto_id, quantidade, preco_unitario) VALUES
+(6, 6, 1, 249.90),    -- 1 Fone Bluetooth
+(6, 8, 2, 99.90);     -- 2 Óculos de Sol
+
+INSERT INTO venda_Produto (venda_id, produto_id, quantidade, preco_unitario) VALUES
+(7, 2, 1, 129.90),    -- 1 Calça Jeans
+(7, 5, 1, 149.90);    -- 1 Relógio Digital
+
+
